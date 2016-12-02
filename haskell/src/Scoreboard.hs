b5070f4346b8ca2c9becf25ee04a5e4136d41a0c
Johannes Link <business@johanneslink.net>
2016-12-02 11:15:19 +0100

    Using lazy IO in input looop

--- haskell/src/Scoreboard.hs
+++ haskell/src/Scoreboard.hs
@@ -2,19 +2,40 @@ module Scoreboard where
 
 import System.IO
 
+data Key = Exit | OtherKey Char
+
+run :: IO()
 run = do
   hSetBuffering stdin NoBuffering
   hSetEcho stdin False
   putStrLn $ "SCOREBOARD started."
-  loop
+  loop getContents
+  -- loop readChars
   putStrLn $ "SCOREBOARD stopped."
   return ()
 
-loop :: IO ()
-loop = do
-  c <- getChar
+readChars :: IO [Char]
+-- Type is correct, but result seems to be never delivered
+readChars = getChar >>= print >> readChars
+
+loop :: IO [Char] -> IO ()
+loop ioChars = do
+  chars <- ioChars
+  let displays = process $ toKeys chars
+  consume displays
+
+consume :: [String] -> IO()
+consume [] = return ()
+consume (aMessage : rest) = do
+  putStrLn aMessage
+  consume rest
+
+toKeys :: [Char] -> [Key]
+toKeys (c : rest) =
   if c == 'x'
-    then return ()
-    else do
-      putStrLn $ "Input: " ++ [c]
-      loop
+    then (Exit : toKeys rest)
+    else (OtherKey c : toKeys rest)
+
+process :: [Key] -> [String]
+process (Exit : _) = []
+process (OtherKey c : rest) = (("Unhandled Input: " ++ [c]) : process rest)
