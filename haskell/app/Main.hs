a5f1e6d8d7d40a66585cf0b44aa9e41e4b0487f4
Johannes Link <business@johanneslink.net>
2016-12-04 16:44:12 +0100

    Simplifying current scoreboard

--- haskell/app/Main.hs
+++ haskell/app/Main.hs
@@ -1,28 +1,28 @@
 module Main where
 
--- import Scoreboard
-import System.IO
-import System.IO.Unsafe
+import Scoreboard
 
--- main :: IO ()
--- main = Scoreboard.run
+main :: IO ()
+main = Scoreboard.run
 
-main = do
-  hSetBuffering stdin NoBuffering
-  hSetEcho stdin False
-  chars <- myGetContents
-  -- chars <- getContents
-  consume chars
-
-consume :: [Char] -> IO ()
-consume [] = return ()
-consume ('x':_) = consume []
-consume (c : rest) = do
-  putStrLn $ "IN: " ++ [c]
-  consume rest
-
-myGetContents :: IO [Char]
-myGetContents = do
-  c <- getChar
-  rest <- unsafeInterleaveIO myGetContents
-  return (c : rest)
+-- import System.IO
+-- import System.IO.Unsafe
+-- main = do
+--   hSetBuffering stdin NoBuffering
+--   hSetEcho stdin False
+--   chars <- myGetContents
+--   -- chars <- getContents
+--   consume chars
+--
+-- consume :: [Char] -> IO ()
+-- consume [] = return ()
+-- consume ('x':_) = consume []
+-- consume (c : rest) = do
+--   putStrLn $ "IN: " ++ [c]
+--   consume rest
+--
+-- myGetContents :: IO [Char]
+-- myGetContents = do
+--   c <- getChar
+--   rest <- unsafeInterleaveIO myGetContents
+--   return (c : rest)
--- haskell/src/Scoreboard.hs
+++ haskell/src/Scoreboard.hs
@@ -2,7 +2,7 @@ module Scoreboard where
 
 import System.IO
 
-data Key = Exit | OtherKey Char
+data Key = Exit | ResetBoard
 
 run :: IO()
 run = do
@@ -10,15 +10,8 @@ run = do
   hSetEcho stdin False
   putStrLn $ "SCOREBOARD started."
   loop getContents
-  -- loop readChars
   putStrLn $ "SCOREBOARD stopped."
   return ()
-
-readChars :: IO [Char]
--- Type is correct, but result seems to be never delivered
-readChars = do
-  c <- getChar
-  readChars >>= \cs -> return (c : cs)
 
 loop :: IO [Char] -> IO ()
 loop ioChars = do
@@ -33,11 +26,10 @@ consume (aMessage : rest) = do
   consume rest
 
 toKeys :: [Char] -> [Key]
-toKeys (c : rest) =
-  if c == 'x'
-    then (Exit : toKeys rest)
-    else (OtherKey c : toKeys rest)
+toKeys ('x' : rest) = (Exit : toKeys rest)
+toKeys ('r' : rest) = (ResetBoard : toKeys rest)
+toKeys (unknown : rest) = toKeys rest
 
 process :: [Key] -> [String]
 process (Exit : _) = []
-process (OtherKey c : rest) = (("Unhandled Input: " ++ [c]) : process rest)
+process (ResetBoard : rest) = ("Resetting " : process rest)
