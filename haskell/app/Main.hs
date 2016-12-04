3b2aa5b7c83f101e0707321fc4de35759f59e030
Johannes Link <business@johanneslink.net>
2016-12-04 12:13:28 +0100

    Added stackoverflow question and answer to main (temporarily)

--- haskell/app/Main.hs
+++ haskell/app/Main.hs
@@ -1,6 +1,28 @@
 module Main where
 
-import Scoreboard
+-- import Scoreboard
+import System.IO
+import System.IO.Unsafe
 
-main :: IO ()
-main = Scoreboard.run
+-- main :: IO ()
+-- main = Scoreboard.run
+
+main = do
+  hSetBuffering stdin NoBuffering
+  hSetEcho stdin False
+  chars <- myGetContents
+  -- chars <- getContents
+  consume chars
+
+consume :: [Char] -> IO ()
+consume [] = return ()
+consume ('x':_) = consume []
+consume (c : rest) = do
+  putStrLn $ "IN: " ++ [c]
+  consume rest
+
+myGetContents :: IO [Char]
+myGetContents = do
+  c <- getChar
+  rest <- unsafeInterleaveIO myGetContents
+  return (c : rest)
