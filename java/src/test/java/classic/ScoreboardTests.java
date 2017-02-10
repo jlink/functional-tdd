f5c14e8085bb0e79f6125f8fbbc1cbff91f2307c
Johannes Link <business@johanneslink.net>
2017-02-10 11:53:53 +0100

    Created JUnit5 gradle project

---
+++ java/src/test/java/classic/ScoreboardTests.java
@@ -0,0 +1,12 @@
+package classic;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ScoreboardTests {
+
+	@Test
+	void firstTest() {
+		Assertions.fail("must fail");
+	}
+}
