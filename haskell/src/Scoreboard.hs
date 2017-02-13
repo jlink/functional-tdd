7a1cb052fae3c87625fbac1eff4ebc0eb0c6c175
Johannes Link <business@johanneslink.net>
2017-02-13 13:47:03 +0100

    Haskell solution according to spec

--- haskell/src/Scoreboard.hs
+++ haskell/src/Scoreboard.hs
@@ -2,36 +2,35 @@ module Scoreboard where
 
 import System.IO
 
-data Key = Exit | ResetBoard | SelectA | SelectB | Score1 | Score2 | Score3
+data Key = Exit | ResetBoard | SelectA | SelectB | Increment | Decrement
   deriving (Show)
 
 run :: IO()
 run = do
-  hSetBuffering stdin NoBuffering
-  hSetEcho stdin False
   putStrLn $ "SCOREBOARD started."
   loop getContents putStrLn
   putStrLn $ "SCOREBOARD stopped."
   return ()
 
-loop :: IO [Char] -> (String -> IO ()) -> IO ()
-loop charReader printer = do
-  chars <- charReader
-  let messages = process newScoreboard $ toKeys chars
-  mapM_ printer messages
+loop :: IO String -> (String -> IO ()) -> IO ()
+loop contentsReader printer = do
+  contents <- contentsReader
+  let commandLines = lines contents
+  let messages = process newScoreboard $ toKeys commandLines
+  mapM_ printer (currentScore newScoreboard : messages)
+  -- mapM_ printer ["OOPS"]
 
-toKeys :: [Char] -> [Key]
-toKeys ('x' : rest) = (Exit : toKeys rest)
-toKeys ('r' : rest) = (ResetBoard : toKeys rest)
-toKeys ('a' : rest) = (SelectA : toKeys rest)
-toKeys ('b' : rest) = (SelectB : toKeys rest)
-toKeys ('1' : rest) = (Score1 : toKeys rest)
-toKeys ('2' : rest) = (Score2 : toKeys rest)
-toKeys ('3' : rest) = (Score3 : toKeys rest)
+toKeys :: [String] -> [Key]
+toKeys ("x" : rest) = (Exit : toKeys rest)
+toKeys ("c" : rest) = (ResetBoard : toKeys rest)
+toKeys ("a" : rest) = (SelectA : toKeys rest)
+toKeys ("b" : rest) = (SelectB : toKeys rest)
+toKeys ("+" : rest) = (Increment : toKeys rest)
+toKeys ("-" : rest) = (Decrement : toKeys rest)
 toKeys (unknown : rest) = toKeys rest
 
 process :: Scoreboard -> [Key] -> [String]
-process scoreboard (Exit : _) = ["Final Score is " ++ currentScore scoreboard]
+process scoreboard (Exit : _) = []
 process scoreboard (key : rest) =
   message command scoreboard nextScoreboard ++ process nextScoreboard rest where
     command = getCommand key
@@ -44,13 +43,12 @@ data Command = Command { operation :: Operation, message :: Message }
 getCommand :: Key -> Command
 getCommand ResetBoard = Command {
   operation = \scoreboard -> newScoreboard,
-  message = \oldScorebaord newScoreboard -> ["Score set to " ++ currentScore newScoreboard]
+  message = \oldScorebaord newScoreboard -> [currentScore newScoreboard]
 }
 getCommand SelectA = createSelectCommand TeamA
 getCommand SelectB = createSelectCommand TeamB
-getCommand Score1 = createScoreCommand 1
-getCommand Score2 = createScoreCommand 2
-getCommand Score3 = createScoreCommand 3
+getCommand Increment = createScoreCommand 1
+getCommand Decrement = createScoreCommand (-1)
 
 createSelectCommand selection = Command {
   operation = \scoreboard -> selectTeam scoreboard selection,
@@ -84,11 +82,10 @@ selectTeam (Scoreboard a b _) s = Scoreboard a b s
 
 score :: Scoreboard -> Score -> Scoreboard
 score (Scoreboard a b None) _ = Scoreboard a b None
-score (Scoreboard a b TeamA) s = Scoreboard (a + s) b None
-score (Scoreboard a b TeamB) s = Scoreboard a (b + s) None
+score (Scoreboard a b TeamA) s = Scoreboard (a + s) b TeamA
+score (Scoreboard a b TeamB) s = Scoreboard a (b + s) TeamB
 
 formatSelection :: Scoreboard -> String
-formatSelection (Scoreboard _ _ None) = "No team selected"
 formatSelection (Scoreboard _ _ TeamA) = "Team A selected"
 formatSelection (Scoreboard _ _ TeamB) = "Team B selected"
 
