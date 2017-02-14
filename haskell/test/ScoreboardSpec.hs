78dd7591d96f6152f810de2ee572165c14fd722f
Johannes Link <business@johanneslink.net>
2017-02-14 12:40:13 +0100

    haskell: added remaining tests for scorboard

--- haskell/test/ScoreboardSpec.hs
+++ haskell/test/ScoreboardSpec.hs
@@ -8,6 +8,20 @@ spec :: Spec
 spec = do
   describe "Scoreboard" $ do
     it "current score of newScoreboard is 0 : 0" $ do
-      let sb = newScoreboard
-      currentScore sb `shouldBe` (0, 0)
-  -- Test rest of scoreboard
+      currentScore newScoreboard `shouldBe` (0, 0)
+    it "in newScoreboard no team is selected" $ do
+      currentSelection newScoreboard `shouldBe` None
+    it "selecting a team" $ do
+      let scoreboardA = selectTeam newScoreboard TeamA
+      currentSelection scoreboardA `shouldBe` TeamA
+      let scoreboardB = selectTeam scoreboardA TeamB
+      currentSelection scoreboardB `shouldBe` TeamB
+    it "incrementing score" $ do
+      let scoreboardA = (Scoreboard (1, 2) TeamA)
+      incrementScore scoreboardA `shouldBe` (Scoreboard (2, 2) TeamA)
+      let scoreboardB = (Scoreboard (1, 2) TeamB)
+      incrementScore scoreboardB `shouldBe` (Scoreboard (1, 3) TeamB)
+    it "dont incrementing or decrement with no team selected" $ do
+      let scoreboard = (Scoreboard (1, 2) None)
+      incrementScore scoreboard `shouldBe` (Scoreboard (1, 2) None)
+      decrementScore scoreboard `shouldBe` (Scoreboard (1, 2) None)
