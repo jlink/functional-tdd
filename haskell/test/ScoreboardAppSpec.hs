a169624e86fcfdcab9fe141f81c5b4f7e4b88b5b
Johannes Link <business@johanneslink.net>
2017-02-14 12:17:12 +0100

    Added remaining tests to ScoreboardAppSpec

--- haskell/test/ScoreboardAppSpec.hs
+++ haskell/test/ScoreboardAppSpec.hs
@@ -43,3 +43,27 @@ spec = do
       let newScoreboard = operation action oldScoreboard
       newScoreboard `shouldBe` (Scoreboard (0, 0) None)
       message action oldScoreboard newScoreboard `shouldBe` "000:000"
+    it "action for Increment will increment score of scoreboard" $ do
+      let action = getAction Increment
+      let oldScoreboard = (Scoreboard (1, 2) TeamA)
+      let newScoreboard = operation action oldScoreboard
+      newScoreboard `shouldBe` (Scoreboard (2, 2) TeamA)
+      message action oldScoreboard newScoreboard `shouldBe` "002:002"
+    it "action for Decrement will decrement score of scoreboard" $ do
+      let action = getAction Decrement
+      let oldScoreboard = (Scoreboard (1, 2) TeamA)
+      let newScoreboard = operation action oldScoreboard
+      newScoreboard `shouldBe` (Scoreboard (0, 2) TeamA)
+      message action oldScoreboard newScoreboard `shouldBe` "000:002"
+    it "action for SelectA will select team A" $ do
+      let action = getAction SelectA
+      let oldScoreboard = (Scoreboard (0, 0) None)
+      let newScoreboard = operation action oldScoreboard
+      newScoreboard `shouldBe` (Scoreboard (0, 0) TeamA)
+      message action oldScoreboard newScoreboard `shouldBe` "Team A selected"
+    it "action for SelectB will select team B" $ do
+      let action = getAction SelectB
+      let oldScoreboard = (Scoreboard (0, 0) None)
+      let newScoreboard = operation action oldScoreboard
+      newScoreboard `shouldBe` (Scoreboard (0, 0) TeamB)
+      message action oldScoreboard newScoreboard `shouldBe` "Team B selected"
