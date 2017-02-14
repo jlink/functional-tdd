929a3de24ce4135ce9cf6258f21738037b6449f6
Johannes Link <business@johanneslink.net>
2017-02-14 12:00:35 +0100

    haskell: separated scoreboard app tests from scoreboard tests

--- haskell/test/ScoreboardSpec.hs
+++ haskell/test/ScoreboardSpec.hs
@@ -7,42 +7,8 @@ import           Test.QuickCheck
 
 spec :: Spec
 spec = do
-  describe "ScoreboardApp.process" $ do
-    it "initial score is 000:000" $ do
-      process newScoreboard [] `shouldBe` ["000:000"]
-
-  describe "ScoreboardApp.formatScore" $ do
-    it "single digit scores are filled in with zeros" $ do
-      formatScore (1, 9) `shouldBe` "001:009"
-    it "multi digit scores are filled in if necessary" $ do
-      formatScore (11, 999) `shouldBe` "011:999"
-    it "more than 3 digits are left alone" $ do
-      formatScore (1234, 98765) `shouldBe` "1234:98765"
-
-  describe "ScoreboardApp.toCommands" $ do
-    it "lines are converted to commands" $ do
-      toCommands ["a", "b", "+", "-", "r", "x"] `shouldBe`
-        [SelectA, SelectB, Increment, Decrement, ResetBoard, Exit]
-    it "lines are sanitized before conversion" $ do
-      toCommands ["   a   ", "B"] `shouldBe` [SelectA, SelectB]
-    it "unknown commands are skipped" $ do
-      toCommands ["a", "z", "ab", "x"] `shouldBe` [SelectA, Exit]
-
-  describe "ScoreboardApp.processCommands" $ do
-    it "commands are processed in order" $ do
-      let messages = processCommands newScoreboard [SelectA, Increment]
-      messages `shouldBe` ["Team A selected", "001:000"]
-    it "Exit ends all command processing" $ do
-      let messages = processCommands newScoreboard [SelectA, Exit, Increment]
-      messages `shouldBe` ["Team A selected"]
-
-  describe "ScoreboardApp.actions" $ do
-    it "action for ResetBoard will reset scoreboard" $ do
-      let action = getAction ResetBoard
-      operation action (Scoreboard (1, 2) TeamA) `shouldBe` (Scoreboard (0, 0) None)
-    -- Test all actions
-
   describe "Scoreboard" $ do
     it "current score of newScoreboard is 0 : 0" $ do
       let sb = newScoreboard
       currentScore sb `shouldBe` (0, 0)
+  -- Test rest of scoreboard
