98319517234f194f465832b4f23f9a3b7558866b
Johannes Link <business@johanneslink.net>
2017-03-08 12:20:02 +0100

    Java: FScoreboardApp.process uses reactivx Observables

--- java/src/main/java/scoreboard/functional/FScoreboardApp.java
+++ java/src/main/java/scoreboard/functional/FScoreboardApp.java
@@ -1,11 +1,20 @@
 package scoreboard.functional;
 
+import io.reactivex.Observable;
+import io.reactivex.ObservableSource;
+import io.reactivex.subjects.BehaviorSubject;
+import io.reactivex.subjects.ReplaySubject;
+import io.reactivex.subjects.Subject;
+import org.reactivestreams.Publisher;
+
 import java.util.Arrays;
 import java.util.stream.Stream;
 
 public class FScoreboardApp {
-	public static Stream<String> process(FScoreboard scoreboard, Stream<String> lines) {
-		// Stream<String> stream = StreamSupport.
-		return Arrays.asList("000:000").stream();
+	public static Observable<String> process(FScoreboard scoreboard, ObservableSource<String> lines) {
+		Subject<String> messages = ReplaySubject.create();
+		messages.onNext("000:000");
+		messages.onComplete();
+		return messages;
 	}
 }
--- java/src/test/java/scoreboard/functional/FunctionalScoreboardTests.java
+++ java/src/test/java/scoreboard/functional/FunctionalScoreboardTests.java
@@ -1,21 +1,24 @@
 package scoreboard.functional;
 
-import static java.util.stream.Collectors.toList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Stream;
+import java.util.concurrent.CountDownLatch;
 
 import org.junit.jupiter.api.Test;
+
+import io.reactivex.Observable;
 
 public class FunctionalScoreboardTests {
 
 	@Test
-	void initialScoreShouldBe000to000() {
-		List<String> lines = new ArrayList<>();
-		Stream<String> messageStream = FScoreboardApp.process(new FScoreboard(), lines.stream());
-		List<String> messages = messageStream.collect(toList());
+	void initialScoreShouldBe000to000() throws InterruptedException {
+		Observable<String> lines = Observable.empty();
+		Observable<String> observable = FScoreboardApp.process(new FScoreboard(), lines);
+		List<String> messages = new ArrayList<>();
+		observable.subscribe(messages::add);
 		assertEquals(1, messages.size());
 		assertEquals("000:000", messages.get(0));
 	}
